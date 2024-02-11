499ed6d15ba98c558b525890196cf0fdae8d7067
lilevil <fuetckpro@gmail.com>
2024-02-11 03:48:19 +0100

    preparing for the integrations tests

--- utils/csp/tests/client.rs
+++ utils/csp/tests/client.rs
@@ -2,5 +2,20 @@ mod common;
 
 #[test]
 fn test_client() {
+    let (thread, lock) = common::listen("127.0.0.1:8080");
 
+    loop {
+        // avoid connecting before the server can accept clients
+        let guard = lock.lock().unwrap();
+        if *guard {
+            break;
+        }
+        // drop guard for us
+    }
+
+    println!("connecting");
+
+    // TODO: send packets and handle responses
+
+    thread.join().unwrap_or_default();
 }
--- utils/csp/tests/common/mod.rs
+++ utils/csp/tests/common/mod.rs
@@ -1,11 +1,33 @@
+use std::{
+    net::{TcpListener, TcpStream},
+    sync::{Arc, Mutex},
+    thread,
+};
 
+// ======================= Server =======================
+pub fn listen(addr: &str) -> (thread::JoinHandle<()>, Arc<Mutex<bool>>) {
+    let mutex = Arc::new(Mutex::new(false));
+    let mutex2 = Arc::clone(&mutex);
 
+    let addr = addr.to_string();
+    (
+        thread::spawn(move || {
+            let mut guard = mutex2.lock().unwrap();
+            let server = TcpListener::bind(addr).unwrap();
+            *guard = true;
+            drop(guard);
+            println!("listen");
 
-
-pub fn setup_server() {
-
+            for client in server.incoming() {
+                if client.is_ok() {
+                    handle_client(client.unwrap())
+                }
+            }
+        }),
+        mutex,
+    )
 }
 
-pub fn lauch_client() {
-
+fn handle_client(client: TcpStream) {
+    // TODO: handle csp parsing and responding
 }
