3e6863984713d7d718dc464e3cbb1049fc85a323
lynn <cedriccolinc@yahoo.com>
2024-02-12 16:05:45 +0100

    Implementation of the Map & the exported map

---
+++ modules/map/src/exported.rs
@@ -0,0 +1,117 @@
+use uuid::Uuid;
+use crate::{Coordinates, NodeType};
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub struct ExportedNode {
+    id: Uuid,
+    node_type: NodeType,
+    coordinates: Coordinates,
+}
+
+impl ExportedNode {
+    pub fn new(id: Uuid, node_type: NodeType, coordinates: Coordinates) -> Self {
+        Self { id, node_type, coordinates }
+    }
+
+    pub fn id(&self) -> Uuid {
+        self.id
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub struct ExportedEdge {
+    source_id: Uuid,
+    target_id: Uuid,
+}
+
+impl ExportedEdge {
+    pub fn new(source_id: Uuid, target_id: Uuid) -> Self {
+        Self { source_id, target_id }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct ExportedMap {
+    nodes: Vec<ExportedNode>,
+    edges: Vec<ExportedEdge>,
+}
+
+impl ExportedMap {
+    pub fn new(nodes: Vec<ExportedNode>, edges: Vec<ExportedEdge>) -> Self {
+        Self { nodes, edges }
+    }
+
+    pub fn get_nodes(&self) -> &Vec<ExportedNode> {
+        &self.nodes
+    }
+
+    pub fn get_edges(&self) -> &Vec<ExportedEdge> {
+        &self.edges
+    }
+}
+
+#[cfg(test)]
+mod exported_map_tests {
+    use uuid::Uuid;
+    use crate::exported::{ExportedEdge, ExportedMap, ExportedNode};
+    use crate::NodeType;
+
+    #[test]
+    fn empty_exported_map(){
+        let m = ExportedMap::default();
+
+        assert_eq!(m.get_edges().len(), 0);
+        assert_eq!(m.get_nodes().len(), 0);
+    }
+
+    #[test]
+    fn only_nodes(){
+        let n1 = ExportedNode::new(Uuid::new_v4(), NodeType::Water, (1, 2).into());
+        let n2 = ExportedNode::new(Uuid::new_v4(), NodeType::Water, (4, 5).into());
+
+        let m = ExportedMap::new(
+            vec![n1.clone(), n2.clone()],
+            Vec::default()
+        );
+
+        assert_eq!(m.get_edges(), &vec![]);
+        assert_eq!(m.get_nodes(), &vec![n1, n2]);
+    }
+
+    #[test]
+    fn only_edges(){
+        let n1 = Uuid::new_v4();
+        let n2 = Uuid::new_v4();
+        let n3  = Uuid::new_v4();
+
+        let e1 = ExportedEdge::new(n1, n2);
+        let e2 = ExportedEdge::new(n2, n3);
+
+        let m = ExportedMap::new(
+            Vec::default(),
+            vec![e1.clone(), e2.clone()]
+        );
+
+        assert_eq!(m.get_edges(), &vec![e1, e2]);
+        assert_eq!(m.get_nodes(), &vec![]);
+    }
+
+    #[test]
+    fn nodes_and_edges(){
+        let n1 = ExportedNode::new(Uuid::new_v4(), NodeType::Water, (1, 2).into());
+        let n2 = ExportedNode::new(Uuid::new_v4(), NodeType::Water, (4, 5).into());
+        let n3 = ExportedNode::new(Uuid::new_v4(), NodeType::Water, (6, 9).into());
+
+
+        let e1 = ExportedEdge::new(n1.id(), n2.id());
+        let e2 = ExportedEdge::new(n2.id(), n3.id());
+
+        let m = ExportedMap::new(
+            vec![n1.clone(), n2.clone(), n3.clone()],
+            vec![e1.clone(), e2.clone()]
+        );
+
+        assert_eq!(m.get_edges(), &vec![e1, e2]);
+        assert_eq!(m.get_nodes(), &vec![n1, n2, n3]);
+    }
+}
--- modules/map/src/lib.rs
+++ modules/map/src/lib.rs
@@ -1,3 +1,203 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+mod exported;
+
+use petgraph::Graph;
+use petgraph::prelude::NodeIndex;
+use uuid::Uuid;
+use crate::exported::{ExportedEdge, ExportedMap, ExportedNode};
+
+#[derive(Debug, Clone, Default, Eq, PartialEq, Ord, PartialOrd, Copy)]
+pub struct Coordinates(i64, i64);
+
+impl Coordinates {
+    pub fn new(x: i64, y: i64) -> Self {
+        Self(x, y)
+    }
+}
+
+impl From<(i64, i64)> for Coordinates {
+    fn from(v: (i64, i64)) -> Self {
+        Self(v.0, v.1)
+    }
+}
+impl From<(i32, i32)> for Coordinates {
+    fn from(v: (i32, i32)) -> Self {
+        Self(v.0 as i64, v.1 as i64)
+    }
+}
+impl From<(usize, usize)> for Coordinates {
+    fn from(v: (usize, usize)) -> Self {
+        Self(v.0 as i64, v.1 as i64)
+    }
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum NodeType {
+    Land(NodeLandType),
+    Water
+}
+
+#[derive(Debug, Clone, Copy, Eq, PartialEq, Ord, PartialOrd, Hash)]
+pub enum NodeLandType {
+    Plain = 0,
+    Forest = 1,
+    Mountain = 2
+}
+
+impl From<NodeLandType> for NodeType {
+    fn from(v: NodeLandType) -> Self {
+        Self::Land(v)
+    }
+}
+
+#[derive(Debug, Clone, Eq, PartialEq, Ord, PartialOrd)]
+pub struct Node {
+    id: Uuid,
+    node_type: NodeType,
+    coordinates: Coordinates
+}
+
+impl Node {
+    pub fn new(t: impl Into<NodeType>, coords: impl Into<Coordinates>, id: Uuid) -> Self {
+        Self {
+            node_type: t.into(),
+            coordinates: coords.into(),
+            id
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Edge(Uuid, Uuid);
+
+impl From<(Uuid, Uuid)> for Edge {
+    fn from(v: (Uuid, Uuid)) -> Self {
+        Self(v.0, v.1)
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct Map {
+    graph: Graph<Node, Edge>
+}
+
+impl Map {
+    pub fn add_node(&mut self, node: impl Into<Node>) -> NodeIndex {
+        self.graph.add_node(node.into())
+    }
+
+    pub fn add_node_mut(mut self, node: impl Into<Node>) -> Self {
+        self.graph.add_node(node.into());
+        self
+    }
+
+    pub fn delete_node(&mut self, node_index: NodeIndex) -> Option<Node> {
+        self.graph.remove_node(node_index)
+    }
+
+    pub fn delete_node_mut(mut self, node_index: NodeIndex) -> Self {
+        self.graph.remove_node(node_index);
+        self
+    }
+
+    pub fn add_edge(&mut self, source: Uuid, target: Uuid) {
+        let source_index = self.node_index_from_id(source).unwrap();
+        let target_index = self.node_index_from_id(target).unwrap();
+        self.graph.add_edge(source_index, target_index, Edge(source, target));
+    }
+
+    pub fn delete_edge(&mut self, source: Uuid, target: Uuid) -> Option<Edge> {
+        let source_index = self.node_index_from_id(source)?;
+        let target_index = self.node_index_from_id(target)?;
+
+        if let Some(edge_index) = self.graph.find_edge(source_index, target_index) {
+            return self.graph.remove_edge(edge_index);
+        }
+
+        None
+    }
+
+    pub fn get_edge(&self, source: Uuid, target: Uuid) -> Option<&Edge> {
+        let source_index = self.node_index_from_id(source)?;
+        let target_index = self.node_index_from_id(target)?;
+
+        if let Some(edge_index) = self.graph.find_edge(source_index, target_index) {
+            return self.graph.edge_weight(edge_index);
+        }
+
+        None
+    }
+
+    pub fn get_node(&self, id: Uuid) -> Option<&Node> {
+        self.graph.node_weight(self.node_index_from_id(id)?)
+    }
+
+    fn node_index_from_id(&self, id: Uuid) -> Option<NodeIndex> {
+        self.graph.node_indices().find(|&index| self.graph[index].id == id)
+    }
+
+    pub fn export(&self) -> ExportedMap {
+        let nodes = self
+            .graph
+            .node_indices()
+            .map(|index| {
+                let node = &self.graph[index];
+                ExportedNode::new(node.id, node.node_type, node.coordinates)
+            })
+            .collect();
+
+        let edges = self
+            .graph
+            .edge_indices()
+            .map(|index| {
+                let edge = &self.graph[index];
+                ExportedEdge::new(edge.0, edge.1)
+            })
+            .collect();
+
+        ExportedMap::new(nodes, edges)
+    }
+}
+
+
+#[cfg(test)]
+mod map_tests {
+    use uuid::Uuid;
+    use crate::{Edge, Map, Node, NodeLandType, NodeType};
+
+    #[test]
+    fn one_node_graph(){
+        let mut m = Map::default();
+
+        let id = Uuid::new_v4();
+        let n = Node::new(NodeType::Water, (6, 5), id);
+        m.add_node(n.clone());
+
+        assert_eq!(m.get_node(id), Some(&n));
+    }
+
+    #[test]
+    fn simple_graph(){
+        let mut m = Map::default();
+
+        let ids = [Uuid::new_v4(), Uuid::new_v4(), Uuid::new_v4()];
+
+        let mut expected_nodes = Vec::new();
+        let mut expected_edges = Vec::new();
+
+        for i in 0..ids.len() {
+            let n = Node::new(NodeType::Land(NodeLandType::Forest), (2 * i, 3 * i + 1), ids[i]);
+            m.add_node(n.clone());
+            expected_nodes.push(n);
+
+            if i > 0 {
+                m.add_edge(ids[i - 1], ids[i]);
+                expected_edges.push(Edge(ids[i - 1], ids[i]))
+            }
+        }
+
+        let exported = m.export();
+
+        assert_eq!(exported.get_nodes().len(), ids.len());
+        assert_eq!(exported.get_edges().len(), ids.len() - 1);
+    }
 }
