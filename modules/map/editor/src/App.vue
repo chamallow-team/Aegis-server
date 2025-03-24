30e2637038b3c5e2ac9a9080745d7ba818b7b497
lynn2910 <cedic2005@gmail.com>
2025-03-24 12:43:20 +0100

    feat(map): Add tailwind in map editor

--- modules/map/editor/src/App.vue
+++ modules/map/editor/src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <header>
+  <header class="bg-black">
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
