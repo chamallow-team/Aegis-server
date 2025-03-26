78b9e151f17be7a1d947f5604aa36619d15e82a7
lynn2910 <cedic2005@gmail.com>
2025-03-26 14:57:53 +0100

    feat: Add RightSide component and integrate it into EditorView

---
+++ modules/map/editor/src/components/general/RightSide.vue
@@ -0,0 +1,6 @@
+<template>
+  <!-- TODO Change width of the side -->
+  <div class="w-56 bg-primary-200/25 m-1 rounded-r-xl h-[calc(100vh-1.5rem)]">
+    <slot></slot>
+  </div>
+</template>
--- modules/map/editor/src/views/editor/EditorView.vue
+++ modules/map/editor/src/views/editor/EditorView.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="flex flex-row h-screen w-screen bg-primary-200/25">
     <EditorNavbar />
-    <div class="overflow-hidden m-2 rounded-xl bg-primary-100 w-full">
+    <div class="overflow-hidden m-2 rounded-xl bg-primary-100 w-full flex flex-row items-start justify-between">
       <router-view name="body"></router-view>
+      <router-view name="right">
+        <RightSide>Test</RightSide>
+      </router-view>
     </div>
   </div>
 </template>
@@ -12,6 +15,7 @@ import { onBeforeMount, onMounted } from 'vue'
 import { useRouter } from 'vue-router'
 import { useEditorStore } from '@/stores/editor.store.ts'
 import EditorNavbar from '@/components/editor/EditorNavbar.vue'
+import RightSide from '@/components/general/RightSide.vue'
 
 const editorStore = useEditorStore()
 const router = useRouter()
